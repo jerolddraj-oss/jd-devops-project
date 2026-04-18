5ccd7ada1e4cd8e4cfa9b531667a4f45d6fc8e2c
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-04-18 14:56:43 +0530

    Updated Terraform with public IP, NSG and Jenkins pipeline changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,12 @@ provider "azurerm" {
   features {}
 }
 
+# Existing Resource Group
 data "azurerm_resource_group" "rg" {
   name = "jd-rg"
 }
 
+# Virtual Network
 resource "azurerm_virtual_network" "vnet" {
   name                = "jd-vnet"
   address_space       = ["10.0.0.0/16"]
@@ -13,6 +15,7 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = data.azurerm_resource_group.rg.name
 }
 
+# Subnet
 resource "azurerm_subnet" "subnet" {
   name                 = "jd-subnet"
   resource_group_name  = data.azurerm_resource_group.rg.name
@@ -20,6 +23,47 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+# Public IP
+resource "azurerm_public_ip" "pip" {
+  name                = "jd-pip"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# NSG with RDP + WinRM
+resource "azurerm_network_security_group" "nsg" {
+  name                = "jd-nsg"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-RDP"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-WinRM"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "5985"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# NIC with Public IP
 resource "azurerm_network_interface" "nic" {
   name                = "jd-nic"
   location            = data.azurerm_resource_group.rg.location
@@ -29,15 +73,23 @@ resource "azurerm_network_interface" "nic" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
   }
 }
 
-# Fetch credentials from Key Vault
+# Attach NSG to NIC
+resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# Key Vault
 data "azurerm_key_vault" "kv" {
   name                = "jd-keyvault"
   resource_group_name = "jd-rg"
 }
 
+# Secrets
 data "azurerm_key_vault_secret" "vm_user" {
   name         = "vm-username"
   key_vault_id = data.azurerm_key_vault.kv.id
@@ -48,15 +100,18 @@ data "azurerm_key_vault_secret" "vm_pass" {
   key_vault_id = data.azurerm_key_vault.kv.id
 }
 
+# Windows VM
 resource "azurerm_windows_virtual_machine" "vm" {
   name                = "jd-vm"
   resource_group_name = data.azurerm_resource_group.rg.name
   location            = data.azurerm_resource_group.rg.location
   size                = "Standard_B2s"
-  admin_username      = data.azurerm_key_vault_secret.vm_user.value
-  admin_password      = data.azurerm_key_vault_secret.vm_pass.value
+
+  admin_username = data.azurerm_key_vault_secret.vm_user.value
+  admin_password = data.azurerm_key_vault_secret.vm_pass.value
+
   network_interface_ids = [
-    azurerm_network_interface.nic.id,
+    azurerm_network_interface.nic.id
   ]
 
   os_disk {
@@ -70,4 +125,13 @@ resource "azurerm_windows_virtual_machine" "vm" {
     sku       = "2019-Datacenter"
     version   = "latest"
   }
+}
+
+# Outputs
+output "vm_public_ip" {
+  value = azurerm_public_ip.pip.ip_address
+}
+
+output "vm_private_ip" {
+  value = azurerm_network_interface.nic.private_ip_address
 }
