eec213d678cdf1c32c4386ab68f48d1631967cbc
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-04-18 15:10:22 +0530

    Removed duplicate outputs from main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -127,11 +127,3 @@ resource "azurerm_windows_virtual_machine" "vm" {
   }
 }
 
-# Outputs
-output "vm_public_ip" {
-  value = azurerm_public_ip.pip.ip_address
-}
-
-output "vm_private_ip" {
-  value = azurerm_network_interface.nic.private_ip_address
-}
