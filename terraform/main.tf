8bdffd691f4130a166e4b4a58e16f3ab8f3cdd44
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-04-18 14:19:55 +0530

    Fix: use existing resource group

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,6 @@ provider "azurerm" {
   features {}
 }
 
-#resource "azurerm_resource_group" "rg" {
-  name     = "jd-rg"
-  location = "East US"
-}
-
 data "azurerm_resource_group" "rg" {
   name = "jd-rg"
 }
