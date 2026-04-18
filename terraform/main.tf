5fc7cc71c736966c704517ca9073801b4b7efb98
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-04-18 14:19:09 +0530

    Fix: use existing resource group

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,21 +2,25 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
+#resource "azurerm_resource_group" "rg" {
   name     = "jd-rg"
   location = "East US"
+}
+
+data "azurerm_resource_group" "rg" {
+  name = "jd-rg"
 }
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "jd-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = data.azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet" {
   name                 = "jd-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
@@ -24,7 +28,7 @@ resource "azurerm_subnet" "subnet" {
 resource "azurerm_network_interface" "nic" {
   name                = "jd-nic"
   location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = data.azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "internal"
@@ -51,7 +55,7 @@ data "azurerm_key_vault_secret" "vm_pass" {
 
 resource "azurerm_windows_virtual_machine" "vm" {
   name                = "jd-vm"
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = data.azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = "Standard_B2s"
   admin_username      = data.azurerm_key_vault_secret.vm_user.value
